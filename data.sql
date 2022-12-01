dca15fde5351508124801d6230d835f5bd8218af
Sebastian Rone <89859504+sebgro98@users.noreply.github.com>
2022-12-01 17:15:40 +0100

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,33 +1,34 @@
-INSERT INTO person (first_name,street,personal_number_UNIQUE,city,zip_code,last_name)
+INSERT INTO person (first_name,street,personal_number,city,zip_code,last_name)
 VALUES
   ('Oliver','Ap #455-6882 Ut Road','1234567891','Lutsel K''e','79095','Andersson');
   
-  INSERT INTO instructor (employ_id_UNIQUE,number_group_lessons,number_of_individual_lessons,instrument_type)
+  INSERT INTO instructor (employ_id,number_group_lessons,number_of_individual_lessons,instrument_type)
 VALUES
   ('43','3','45','drums');
   
-INSERT INTO student (skill_level,relative_name, relative_phone_number,number_of_rented_instruments,student_number_UNIQUE,number_of_lessons_taken,instrument_type)
+INSERT INTO student (skill_level,relative_name, relative_phone_number,number_of_rented_instruments,student_number,number_of_lessons_taken,instrument_type)
 VALUES
   ('beginner','Carl','0123345678','1','012334456','4','drums');
   
 INSERT INTO discount (discount_procent)
 VALUES
   ('10');
-INSERT INTO siblings (relation, student_id_UNIQUE, number_of_siblings,instrument_type)
+INSERT INTO siblings (relation, school_id)
 
 VALUES
-  ('2','9421412','3','drums');
-INSERT INTO phone_number (phone_number_UNIQUE)
+  ('2','9421412');
+INSERT INTO phone_number (phone_number)
 VALUES
   ('789421904');
   
-INSERT INTO email (email_UNIQUE)
+INSERT INTO email (email)
 VALUES
   ('FJHASKFDAS@GMAIL.COM');
   
-INSERT INTO lessons (skill_level,time_and_date)
+INSERT INTO lessons (skill_level, start_date, end_date)
 VALUES
-  ('intermediate', '1000-01-01');
+  ('intermediate', '1000-01-01', '1000-01-01');
+  
     INSERT INTO scheduled_timeslots (time_slot,time_and_date)
 VALUES
   ('1000-01-01', '1000-01-01');
@@ -36,7 +37,11 @@ VALUES
 VALUES
   ('1000-01-01', 'Carl');
   
-  INSERT INTO prices (beginner_and_intermediate,advanced,group_lesson,individual_lesson)
+  INSERT INTO prices (price_beginner_and_intermediate, price_advanced, price_group_lesson, price_individual_lesson, price_for_instrument)
+VALUES
+  ('100','200','500','80', '100');
+  
+  INSERT INTO leasings_instruments (leasing_period,number_of_instruments,in_stock, delivery)
 VALUES
   ('100','200','500','80');
   
